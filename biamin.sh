42f1b17947429e324202b63020772622731c8398
kstn <kastian@mail.ru>
2014-06-24 15:22:29 +0400

    MINOR formatting comments

--- biamin.sh
+++ biamin.sh
@@ -990,8 +990,8 @@ BiaminSetup() { # Used in MainMenu()
 	SaveCurrentSheet
     fi # Finish check whether CHAR exists if not create CHARSHEET 
     sleep 2 # merged sleep from 'load char' and 'new char'
-    # Set abilities according to race (each equal to 12)
-    case $CHAR_RACE in                                     # original:         # new: change from D6 to D20 rolls
+    # Set abilities according to race (each equal to 12 except FLEE)
+    case $CHAR_RACE in                                         # original:         # new: change from D6 to D20 rolls
 	1 ) HEALING=3 && STRENGTH=3 && ACCURACY=3 && FLEE=8 ;; # human  (3,3,3,3)  # was 50% is now 40%
 	2 ) HEALING=4 && STRENGTH=3 && ACCURACY=4 && FLEE=4 ;; # elf    (4,3,4,1)  # was 16% is now 20%
 	3 ) HEALING=2 && STRENGTH=5 && ACCURACY=3 && FLEE=7 ;; # dwarf  (2,5,3,2)  # was 33% is now 35%

5bfa1676feb2739ffa06dcf0362fe71365c26bda
kstn <kastian@mail.ru>
2014-11-08 16:38:30 +0300

    BUGFIX: remove forgotten debug options

--- runtime.sh
+++ runtime.sh
@@ -3,8 +3,6 @@
 #                                                                      #
 #                        2. RUNTIME BLOCK                              #
 #                   All running code goes here!                        #
-GX_Races
-read
 
 # Make place for game (BEFORE CLI opts! Mostly because of Higscore and MapCreateCustom())
 if [[ ! -d "$GAMEDIR" ]] ; then # Check whether gamedir exists...

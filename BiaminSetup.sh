a6d09030ad750c4c30dfb3a7dbb642b45acf8404
kstn <kastian@mail.ru>
2014-11-09 05:18:49 +0300

    Move BiaminSetup_UpdateOldSaves() to separate functions and small fixes

--- BiaminSetup.sh
+++ BiaminSetup.sh
@@ -32,77 +32,90 @@ BiaminSetup_SetItemsAbilities() {
     HaveItem "$STEADY_HAND_BREW"      && ((ACCURACY++))
 }
 
-BiaminSetup_LoadCharsheet() {
-	echo -en " Welcome back, $CHAR!\n Loading character sheet ..." # -n for 80x24, DO NOT REMOVE IT #kstn	
-	# Sequence for updating older charsheets to later additions (compatibility)
-	grep -Eq '^HOME:' "$CHARSHEET"        || echo "HOME: $START_LOCATION" >> $CHARSHEET
-	grep -Eq '^GOLD:' "$CHARSHEET"        || echo "GOLD: 10" >> $CHARSHEET
-	grep -Eq '^TOBACCO:' "$CHARSHEET"     || echo "TOBACCO: 10" >> $CHARSHEET
-	grep -Eq '^FOOD:' "$CHARSHEET"        || echo "FOOD: 10" >> $CHARSHEET
-	grep -Eq '^BBSMSG:' "$CHARSHEET"      || echo "BBSMSG: 0" >> $CHARSHEET
-	grep -Eq '^STARVATION:' "$CHARSHEET"  || echo "STARVATION: 0" >> $CHARSHEET
-	# TODO use  OFFSET_{GOLD,TOBACCO} 
-	grep -Eq '^VAL_GOLD:' "$CHARSHEET"    || echo "VAL_GOLD: 1" >> $CHARSHEET
-	grep -Eq '^VAL_TOBACCO:' "$CHARSHEET" || echo "VAL_TOBACCO: 1" >> $CHARSHEET
-	grep -Eq '^VAL_CHANGE:' "$CHARSHEET"  || echo "VAL_CHANGE: 0.25" >> $CHARSHEET
-	# Time 
-	grep -Eq '^TURN:' "$CHARSHEET"        || echo "TURN: $(TurnFromDate)" >> $CHARSHEET
-	# Almanac
-	grep -Eq '^ALMANAC:' "$CHARSHEET"     || echo "ALMANAC: 0" >> $CHARSHEET
-	# TODO I don't know why, but "read -r VAR1 VAR2 VAR3 <<< $(awk $FILE)" not works :(
-	# But one local variable at any case is better that to open one file eight times
-	local CHAR_TMP=$(awk '
-                  { 
-                   if (/^CHARACTER:/)  { RLENGTH = match($0,/: /);
-                  	                 CHARACTER = substr($0, RLENGTH+2); }
-                   if (/^RACE:/)       { RACE= $2 }
-                   if (/^BATTLES:/)    { BATTLES = $2 }
-                   if (/^EXPERIENCE:/) { EXPERIENCE = $2 }
-                   if (/^LOCATION:/)   { LOCATION = $2 }
-                   if (/^HEALTH:/)     { HEALTH = $2 }
-                   if (/^ITEMS:/)      { ITEMS = $2 }
-                   if (/^KILLS:/)      { KILLS = $2 }
-                   if (/^HOME:/)       { HOME = $2 }
-                   if (/^GOLD:/)       { GOLD = $2 }
-                   if (/^TOBACCO:/)    { TOBACCO = $2 }
-                   if (/^FOOD:/)       { FOOD = $2 }
-                   if (/^BBSMSG:/)     { BBSMSG = $2 }
-                   if (/^VAL_GOLD:/)   { VAL_GOLD = $2 }
-                   if (/^VAL_TOBACCO:/){ VAL_TOBACCO = $2 }
-                   if (/^VAL_CHANGE:/) { VAL_CHANGE = $2 }
-                   if (/^STARVATION:/) { STARVATION = $2 }
-                   if (/^TURN:/)        { TURN= $2 }
-                   if (/^ALMANAC:/)     { ALMANAC = $2 }
-                 }
-                 END { 
-                 print CHARACTER ";" RACE ";" BATTLES ";" EXPERIENCE ";" LOCATION ";" HEALTH ";" ITEMS ";" KILLS ";" HOME ";" GOLD ";" TOBACCO ";" FOOD ";" BBSMSG ";" VAL_GOLD ";" VAL_TOBACCO ";" VAL_CHANGE ";" STARVATION ";" TURN ";" ALMANAC ";"
-                 }' $CHARSHEET )
-	IFS=";" read -r CHAR CHAR_RACE CHAR_BATTLES CHAR_EXP CHAR_GPS CHAR_HEALTH CHAR_ITEMS CHAR_KILLS CHAR_HOME CHAR_GOLD CHAR_TOBACCO CHAR_FOOD BBSMSG VAL_GOLD VAL_TOBACCO VAL_CHANGE STARVATION TURN ALMANAC <<< "$CHAR_TMP"
-	unset CHAR_TMP
-	# If character is dead, don't fool around..
-	(( CHAR_HEALTH <= 0 )) && Die "\nWhoops!\n $CHAR's health is $CHAR_HEALTH!\nThis game does not support necromancy, sorry!"
+#-----------------------------------------------------------------------
+# BiaminSetup_UpdateOldSaves()
+# Sequence for updating older charsheets to later additions (compatibility)
+# Arguments: $CHARSHEET(string)
+# Used: BiaminSetup_LoadCharsheet()
+#-----------------------------------------------------------------------
+BiaminSetup_UpdateOldSaves() {	
+    grep -Eq '^HOME:' "$1"        || echo "HOME: $START_LOCATION" >> $1
+    grep -Eq '^GOLD:' "$1"        || echo "GOLD: 10" >> $1
+    grep -Eq '^TOBACCO:' "$1"     || echo "TOBACCO: 10" >> $1
+    grep -Eq '^FOOD:' "$1"        || echo "FOOD: 10" >> $1
+    grep -Eq '^BBSMSG:' "$1"      || echo "BBSMSG: 0" >> $1
+    grep -Eq '^STARVATION:' "$1"  || echo "STARVATION: 0" >> $1
+    # TODO use  OFFSET_{GOLD,TOBACCO} 
+    grep -Eq '^VAL_GOLD:' "$1"    || echo "VAL_GOLD: 1" >> $1
+    grep -Eq '^VAL_TOBACCO:' "$1" || echo "VAL_TOBACCO: 1" >> $1
+    grep -Eq '^VAL_CHANGE:' "$1"  || echo "VAL_CHANGE: 0.25" >> $1
+    # Time 
+    grep -Eq '^TURN:' "$1"        || echo "TURN: $(TurnFromDate)" >> $1
+    # Almanac
+    grep -Eq '^ALMANAC:' "$1"     || echo "ALMANAC: 0" >> $1
 }
 
-BiaminSetup_SanityCheck() {
-    if [[ ! -z "$CHAR_LOC" ]]; then
-	# Use user input as start location.. but first SANITY CHECK
-	read CHAR_LOC_LEN CHAR_LOC_A CHAR_LOC_B <<< $(awk '{print length($0) " " substr($0,0,1) " " substr($0,2)}' <<< "$CHAR_LOC")
-	(( CHAR_LOC_LEN < 1 )) && Die " Error! Too less characters in $CHAR_LOC\n Start location is 2-3 alphanumeric chars [A-R][1-15], e.g. C2 or P13"
-	(( CHAR_LOC_LEN > 3 )) && Die " Error! Too many characters in $CHAR_LOC\n Start location is 2-3 alphanumeric chars [A-R][1-15], e.g. C2 or P13"
-	echo -n "Sanity check.."
-	case "$CHAR_LOC_A" in
-	    A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R ) echo -n ".." ;;
-	    * ) Die "\n Error! Start location X-Axis $CHAR_LOC_A must be a CAPITAL alphanumeric A-R letter!" ;;
-	esac
-	case "$CHAR_LOC_B" in
-	    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 ) echo ".. Done!" ;;
-	    * ) Die "\n Error! Start location Y-Axis $CHAR_LOC_B is too big or too small!";;
-	esac
-	# End of SANITY check, everything okay!
-	CHAR_GPS="$CHAR_LOC"
-	CHAR_HOME="$CHAR_LOC"
-	unset CHAR_LOC CHAR_LOC_LEN CHAR_LOC_A CHAR_LOC_B
-    fi # or CHAR_GPS and CHAR_HOME not changed from START_LOCATION
+#-----------------------------------------------------------------------
+# BiaminSetup_SanityCheck()
+# Checks if $1 in GPS format ([A-R][1-15])
+# Arguments: $CHAR_LOC(string)
+# Used: BiaminSetup_LoadCharsheet()
+#-----------------------------------------------------------------------
+BiaminSetup_SanityCheck(){
+    local CHAR_LOC_LEN CHAR_LOC_X CHAR_LOC_Y
+    read CHAR_LOC_LEN CHAR_LOC_X CHAR_LOC_Y <<< $(awk '{print length($0) " " substr($0,0,1) " " substr($0,2)}' <<< "$1")
+    (( CHAR_LOC_LEN < 1 )) && Die " Error! Too less characters in $CHAR_LOC\n Start location is 2-3 alphanumeric chars [A-R][1-15], e.g. C2 or P13"
+    (( CHAR_LOC_LEN > 3 )) && Die " Error! Too many characters in $CHAR_LOC\n Start location is 2-3 alphanumeric chars [A-R][1-15], e.g. C2 or P13"
+    echo -n "Sanity check.."
+    case "$CHAR_LOC_X" in
+	A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R ) echo -n ".." ;;
+	* ) Die "\n Error! Start location X-Axis $CHAR_LOC_X must be a CAPITAL alphanumeric A-R letter!" ;;
+    esac
+    case "$CHAR_LOC_Y" in
+	1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 ) echo ".. Done!" ;;
+	* ) Die "\n Error! Start location Y-Axis $CHAR_LOC_Y is too big or too small!";;
+    esac
+    # End of SANITY check, everything okay!
+    CHAR_GPS="$1"
+    CHAR_HOME="$1"
+}
+
+BiaminSetup_LoadCharsheet() {
+    echo -en " Welcome back, $CHAR!\n Loading character sheet ..." # -n for 80x24, DO NOT REMOVE IT #kstn	
+    BiaminSetup_UpdateOldSaves "$CHARSHEET"
+    IFS=";" read -r CHAR CHAR_RACE CHAR_BATTLES CHAR_EXP CHAR_GPS \
+       CHAR_HEALTH CHAR_ITEMS CHAR_KILLS CHAR_HOME CHAR_GOLD CHAR_TOBACCO \
+       CHAR_FOOD BBSMSG VAL_GOLD VAL_TOBACCO VAL_CHANGE STARVATION TURN ALMANAC <<< $(awk '
+       {
+           if (/^CHARACTER:/)  { RLENGTH = match($0,/: /);
+              	                 CHARACTER = substr($0, RLENGTH+2); }
+           if (/^RACE:/)       { RACE= $2 }
+           if (/^BATTLES:/)    { BATTLES = $2 }
+           if (/^EXPERIENCE:/) { EXPERIENCE = $2 }
+           if (/^LOCATION:/)   { LOCATION = $2 }
+           if (/^HEALTH:/)     { HEALTH = $2 }
+           if (/^ITEMS:/)      { ITEMS = $2 }
+           if (/^KILLS:/)      { KILLS = $2 }
+           if (/^HOME:/)       { HOME = $2 }
+           if (/^GOLD:/)       { GOLD = $2 }
+           if (/^TOBACCO:/)    { TOBACCO = $2 }
+           if (/^FOOD:/)       { FOOD = $2 }
+           if (/^BBSMSG:/)     { BBSMSG = $2 }
+           if (/^VAL_GOLD:/)   { VAL_GOLD = $2 }
+           if (/^VAL_TOBACCO:/){ VAL_TOBACCO = $2 }
+           if (/^VAL_CHANGE:/) { VAL_CHANGE = $2 }
+           if (/^STARVATION:/) { STARVATION = $2 }
+           if (/^TURN:/)        { TURN= $2 }
+           if (/^ALMANAC:/)     { ALMANAC = $2 }
+       }       
+       END { 
+           print CHARACTER ";" RACE ";" BATTLES ";" EXPERIENCE ";" LOCATION ";" \
+              	HEALTH ";" ITEMS ";" KILLS ";" HOME ";" GOLD ";" TOBACCO ";" FOOD ";" \
+              	BBSMSG ";" VAL_GOLD ";" VAL_TOBACCO ";" VAL_CHANGE ";" STARVATION ";" \
+              	TURN ";" ALMANAC ";" 
+       }' "$CHARSHEET" )
+    # If character is dead, don't fool around..
+    (( CHAR_HEALTH <= 0 )) && Die "\nWhoops!\n $CHAR's health is $CHAR_HEALTH!\nThis game does not support necromancy, sorry!"
 }
 
 # BiaminSetup_MakeBaseChar() {
@@ -159,10 +172,8 @@ BiaminSetup_MakeNewChar() {
 	esac
 
 	# WEALTH formula = D20 + (D6 * CLASS OFFSET)
-	CHAR_GOLD=$(    bc <<< "$(RollDice2 20) + ($OFFSET_GOLD    * $(RollDice2 6))" )
-	CHAR_TOBACCO=$( bc <<< "$(RollDice2 20) + ($OFFSET_TOBACCO * $(RollDice2 6))" )
-	(( CHAR_TOBACCO < 0 )) && CHAR_TOBACCO=0 # healthy bastard
-	(( CHAR_GOLD < 0 ))    && CHAR_GOLD=0    # poor bastard
+	CHAR_GOLD=$(    bc <<< "var = ( $(RollDice2 20) + ($OFFSET_GOLD    * $(RollDice2 6)) ); if (var > 0) var else 0" )
+	CHAR_TOBACCO=$( bc <<< "var = ( $(RollDice2 20) + ($OFFSET_TOBACCO * $(RollDice2 6)) ); if (var > 0) var else 0" )
 
 	# Determine initial food stock (D16 + 4) - player has 5 food minimum
 	CHAR_FOOD=$( bc <<< "$(RollDice2 16) + 4" )
@@ -178,13 +189,12 @@ BiaminSetup_MakeNewChar() {
 	# TODO move it to LoadCustomMap()
 	if [[ "$CUSTOM_MAP" ]] ; then
 	    START_LOCATION=$(awk '{ if (/^START LOCATION:/) { print $2; exit; } print "'$START_LOCATION'"; }' <<< "$CUSTOM_MAP" )
-	    read -p " HOME location for custom maps (ENTER for default $START_LOCATION): " "CHAR_LOC" 2>&1
-	    BiaminSetup_SanityCheck
+	    read -p " HOME location for custom maps (ENTER for default $START_LOCATION): " CHAR_LOC 2>&1
+	    [[ ! -z "$CHAR_LOC" ]] && BiaminSetup_SanityCheck "$CHAR_LOC" 	# Use user input as start location.. but first SANITY CHECK
 	fi
 	echo " Creating fresh character sheet for $CHAR ..."
 	SaveCurrentSheet
 }
-
 
 #-----------------------------------------------------------------------
 # BiaminSetup()
@@ -195,11 +205,7 @@ BiaminSetup_MakeNewChar() {
 BiaminSetup() { 
     # Set CHARSHEET variable to gamedir/char.sheet (lowercase)
     CHARSHEET="$GAMEDIR/$(echo "$CHAR" | tr '[:upper:]' '[:lower:]' | tr -d " ").sheet"
-    if [[ -f "$CHARSHEET" ]]; then
-	BiaminSetup_LoadCharsheet
-    else
-	BiaminSetup_MakeNewChar
-    fi
+    [[ -f "$CHARSHEET" ]] && BiaminSetup_LoadCharsheet || BiaminSetup_MakeNewChar
     sleep 2
     BiaminSetup_SetRaceAbilities  "$CHAR_RACE"
     BiaminSetup_SetItemsAbilities "$CHAR_ITEMS"

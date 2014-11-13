1f4a85f54a41beb891a66f9c2d237cbb95773f4e
kstn <kastian@mail.ru>
2014-11-13 10:38:07 +0300

    FIX Read()

--- Read.sh
+++ Read.sh
@@ -3,8 +3,11 @@
 # Flush 512 symbols readed before and read one symbol
 #-----------------------------------------------------------------------    
 Read() {
-    read -s -t 0.001 -n 512 
-    read -s -n 1 VAR
-    echo "$VAR"
+    # read -s -t 0.001 -n 512
+    # read -s -n 1 VAR
+    # echo "$VAR"
+    read -s -t 0 -n 512 	# Flush 512 symbols in in buffer
+    read -s -n 1		# Read only one symbol (to default variable, $REPLY)
+    echo "$REPLY"		# And echo it
 }
 

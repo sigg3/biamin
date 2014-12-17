0fa7409aa3f3fa4ab3e9f3d29c8212b31ed6a1f4
kstn <kastian@mail.ru>
2014-12-17 21:53:34 +0300

    BUGFIX HighscoreRead()

--- Menu.sh
+++ Menu.sh
@@ -55,7 +55,7 @@ LoadGame() {
 
 HighscoreRead() { # Used in Announce() and HighScore()
     sort -g -r "$HIGHSCORE" -o "$HIGHSCORE"
-    local HIGHSCORE_TMP=" #;Hero;EXP;Wins;Items;Entered History\n;"
+    local HIGHSCORE_TMP=" #;Hero;EXP;Wins;Items;Entered History\n"
     local i=1
     # Read values from highscore file (BashFAQ/001)
     while IFS=";" read -r highEXP highCHAR highRACE highBATTLES highKILLS highITEMS highDATE highMONTH highYEAR; do

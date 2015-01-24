8cf05dad8dd9d6c9e828faa8926e513527b8cb3b
sigg3 <biamin@sigg3.net>
2015-01-24 21:56:10 +0100

    NOTES for Inventory system

--- GameChar.sh
+++ GameChar.sh
@@ -2,7 +2,22 @@
 #                      All char-related functions                      #
 #                                                                      #
 
+# SKETCH for Inventory Index (TODO 2.n release)
+#
+# Read inventory from $INVENTORY array
+#FOOD="${INVENTORY[0]}"
+#GOLD="${INVENTORY[1]}"
+#TOBACCO="${INVENTORY[2]}"
+#ALMANAC="${INVENTORY[3]}"
+#POTION_HEALTH_5="${INVENTORY[4]}"
+#POTION_HEALTH_10="${INVENTORY[5]}"
+#POTION_HEALTH_15="${INVENTORY[6]}"
+#POTION_HEALTH_25="${INVENTORY[7]}"
+#POTION_HEALTH_50="${INVENTORY[8]}"
 
+
+# Save Inventory to $INVENTORY array
+# TODO (2.n release)
 
 
 

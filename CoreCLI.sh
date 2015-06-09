db7621594090cb77fe14f588c8f144371f7997f9
sigg3 <biamin@sigg3.net>
2015-06-09 19:12:16 +0200

    CoreCLI.sh added help instructions

--- CoreCLI.sh
+++ CoreCLI.sh
@@ -164,6 +164,9 @@ CLI_Help() {
     echo "For usage: run biamin --usage"
     echo "Current dir for game files: $GAMEDIR/"
     echo "Change at runtime or on line 10 in the CONFIGURATION of the script."
+    echo -e "\nReport bugs to: <http://sigg3.net/biamin/bugs>"
+	echo "Home page: <http://sigg3.net/biamin/>"
+  # echo "General help using GNU software: <http://www.gnu.org/gethelp/>" # TODO create a youtube + pic tutorial
     Exit 0
 }
 
